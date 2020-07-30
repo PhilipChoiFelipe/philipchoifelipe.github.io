edeb1460476f58f35df2d674725e92d67a81f5d9
PhilipChoiFelipe <mschoi16@uw.edu>
2020-07-30 07:37:41 +0000

    Updates

---
+++ precache-manifest.ce058fd7a7e5e3a78b93a719565dc92f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4324f4a626474e0ecf1f78041fa78984",
+    "url": "/static/media/JSDongkang-Regular.4324f4a6.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e51cbadd7108fc6a8372",
+    "url": "/static/js/main.e51cbadd.chunk.js"
+  },
+  {
+    "revision": "dd60c096e3d5f40b9079",
+    "url": "/static/js/1.dd60c096.chunk.js"
+  },
+  {
+    "revision": "e51cbadd7108fc6a8372",
+    "url": "/static/css/main.35fcb93d.chunk.css"
+  },
+  {
+    "revision": "dd60c096e3d5f40b9079",
+    "url": "/static/css/1.f6cd8ac6.chunk.css"
+  },
+  {
+    "revision": "6ffd020e668713ac12752451aa2c55e7",
+    "url": "/index.html"
+  }
+];
